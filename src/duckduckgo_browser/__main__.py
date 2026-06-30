9e51559c40371d4052ee3c0bbf73f51ac3924dcc
sourav-spd <souravdas46071114@gmail.com>
2026-06-30 15:46:15 +0530

    Delete src/duckduckgo_browser directory

--- src/duckduckgo_browser/__main__.py
+++
@@ -1,5 +0,0 @@
-import asyncio
-from .duckduckgo_server import main
-
-if __name__ == "__main__":
-    asyncio.run(main())
--- src/duckduckgo_browser/tools/__init__.py
+++
@@ -1,4 +0,0 @@
-from duckduckgo_browser.tools.search_tools import GetInternetResultToolHandler
-from duckduckgo_browser.tools.toolhandler import ToolHandler
-
-__all__ = ["ToolHandler", "GetInternetResultToolHandler"]
